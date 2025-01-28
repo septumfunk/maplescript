72d55e69f5b07b14965b7e7da8973dd2d30c5fec
james <soapy.dev@proton.me>
2025-01-28 08:02:44 -0800

    Ohio Skibitty

---
+++ Functions.cmake
@@ -0,0 +1,12 @@
+include(CheckCCompilerFlag)
+
+function(compiler_flag flag)
+    string(FIND "${CMAKE_C_FLAGS}" "${flag}" flag_already_set)
+    if(flag_already_set EQUAL -1)
+        check_c_compiler_flag("${flag}" flag_supported)
+        if(flag_supported)
+            set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} ${flag}" PARENT_SCOPE)
+        endif()
+        unset(flag_supported CACHE)
+    endif()
+endfunction()
