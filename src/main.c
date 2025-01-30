0a6208f2a3517386bec7a1eb3d24e1454c60cdb2
james <soapy.dev@proton.me>
2025-01-30 13:41:32 -0800

    yeah

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Lol\n");
+    int array[10] = {};
+    int array2[12] = {};
+
+    for (int i = 0; i < 15; ++i)
+        array[i] = i;
+
+    for (int i = 0; i < 10; ++i)
+        printf("%d ", array[i]);
+    puts("\n");
+    for (int i = 0; i < 12; ++i)
+        printf("%d ", array2[i]);
+    puts("\n");
     return 0;
 }
